d6042954c5c4d3c7c5c40fe59f761785320f21f3
Maria Nikitina <nikma1108@mail.ru>
2022-06-18 09:59:04 +0300

    The code was reformated. New tests were added

--- src/main/java/com/ya/Account.java
+++ src/main/java/com/ya/Account.java
@@ -9,30 +9,13 @@ public class Account {
 
     public boolean checkNameToEmboss() {
 
-        if(name!=null&&
-                ((!name.startsWith(" "))&&(!name.endsWith(" "))&&
-                (name.length()-name.replaceAll(" ","").length()==1)&&
-                ((name.length()>=3)&&(name.length()<=19)))){
+        if (name != null &&
+                !name.startsWith(" ") && !name.endsWith(" ") &&
+                name.length() - name.replaceAll(" ", "").length() == 1 &&
+                name.length() >= 3 && name.length() <= 19) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
-       /* if((name.length()<3)||(name.length()>19)){
-            return false;
-        }
-        else if ((name.startsWith(" "))||(name.endsWith(" "))){
-            return false;
-        }
-        else if (name.length()-name.replaceAll(" ","").length()!=1){
-            return false;
-        }
-        else{return true;}
-*/
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
-
     }
 }
